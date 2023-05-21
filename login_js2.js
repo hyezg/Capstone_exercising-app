a5afd1deb6ee9b38b9fd2c278bdac3525c58b300
kimhss <hyunuary108@kyonggi.ac.kr>
2023-05-22 05:06:40 +0900

    login view_1

--- login_js2.js
+++ login_js2.js
@@ -2,7 +2,7 @@
 
 const loginId = document.getElementById('signUpEmail');
 const loginPw = document.getElementById('signUpPassword');
-const loginBtn = document.getElementById('signInButton');
+const loginBtn = document.getElementById('signUpButton');
 
 function color() {
     if((loginId.value.length>0 && loginId.value.indexOf("@")!==-1) 
@@ -15,10 +15,11 @@ function color() {
     }
 }
 
-function moveToMain(){
-    location.replace("main.html");
+function moveToBack(){
+    alert("회원가입이 완료되었습니다.");
+    location.replace("join.html");
 }
 
 loginId.addEventListener('keyup', color);
 loginPw.addEventListener('keyup', color);
-loginBtn.addEventListener('click',moveToMain);
+loginBtn.addEventListener('click',moveToBack);
